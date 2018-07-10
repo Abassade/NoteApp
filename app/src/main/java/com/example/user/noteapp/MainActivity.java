c302da5a62d5149a286d9dea3c4ac7ecfb8018f3
Abass <abs4real16.ma@gmail.com>
2018-07-09 17:56:01 -0700

    UI pass

--- app/src/main/java/com/example/user/noteapp/MainActivity.java
+++ app/src/main/java/com/example/user/noteapp/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.user.noteapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // sdghsbgkjhsnfk
+
+        fab_add = findViewById(R.id.fab_add);
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
