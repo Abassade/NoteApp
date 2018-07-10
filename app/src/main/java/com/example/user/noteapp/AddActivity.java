66301a47ad35570a01f5cf1c37f692ead0aa8933
Abass <abs4real16.ma@gmail.com>
2018-07-09 18:53:58 -0700

    UI pass

---
+++ app/src/main/java/com/example/user/noteapp/AddActivity.java
@@ -0,0 +1,55 @@
+package com.example.user.noteapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class AddActivity extends AppCompatActivity {
+
+    private LinearLayout parent;
+    private EditText Edtitle, Edbody;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        parent = findViewById(R.id.parent);
+        Edtitle = findViewById(R.id.title);
+        Edbody = findViewById(R.id.body);
+
+
+
+
+        parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                InputMethodManager methodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                assert methodManager != null;
+                methodManager.hideSoftInputFromWindow(Edbody.getWindowToken(), 0);
+                methodManager.hideSoftInputFromWindow(Edtitle.getWindowToken(), 1);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
