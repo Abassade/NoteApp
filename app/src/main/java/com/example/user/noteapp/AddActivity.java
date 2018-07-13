d5024003c500711f06206001d628208fd6063c92
Abass <abs4real16.ma@gmail.com>
2018-07-13 10:26:42 -0700

    UI pass_second

--- app/src/main/java/com/example/user/noteapp/AddActivity.java
+++ app/src/main/java/com/example/user/noteapp/AddActivity.java
@@ -1,15 +1,18 @@
 package com.example.user.noteapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -46,18 +49,41 @@ public class AddActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.add_menu, menu);
+
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        // To navigate back to the root
-        if (item.getItemId()==android.R.id.home){
+        int menu_id = item.getItemId();
 
+        switch (menu_id){
+
+            case android.R.id.home:
             NavUtils.navigateUpFromSameTask(this);
+            break;
+            case R.id.add_note:
+                Toast.makeText(getBaseContext(), "Note added", Toast.LENGTH_SHORT).show();
+                nextActivity();
+                break;
+            case R.id.lock_note:
+                Toast.makeText(getBaseContext(), "Note locked", Toast.LENGTH_SHORT).show();
+                nextActivity();
+                break;
+            case R.id.delete_note:
+                Toast.makeText(getBaseContext(), "Note deleted", Toast.LENGTH_SHORT).show();
+                nextActivity();
+                break;
+                default:return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void nextActivity(){
+        Intent intent = new Intent(AddActivity.this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
