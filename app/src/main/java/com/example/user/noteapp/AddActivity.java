d4f19435e0f2b9f71971ddf2694c26a2abd78a05
Abass <abs4real16.ma@gmail.com>
2018-08-07 10:01:05 -0800

    Note initial

--- app/src/main/java/com/example/user/noteapp/AddActivity.java
+++ app/src/main/java/com/example/user/noteapp/AddActivity.java
@@ -1,10 +1,13 @@
 package com.example.user.noteapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,12 +15,19 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddActivity extends AppCompatActivity {
 
     private LinearLayout parent;
     private EditText Edtitle, Edbody;
+    private TextView mydate;
+    private   String mFilename;
+    private Note loadedNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +37,30 @@ public class AddActivity extends AppCompatActivity {
         parent = findViewById(R.id.parent);
         Edtitle = findViewById(R.id.title);
         Edbody = findViewById(R.id.body);
+        mydate = findViewById(R.id.mydate);
 
+       SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy . hh:mm:ss aa");
+       mydate.setText(String.valueOf(dateFormat.format(new Date())));
+        mFilename = getIntent().getStringExtra("file_me");
 
+        if (mFilename!=null){
 
+            loadedNote = Utilitties.getNoteByName(this, mFilename);
 
-        parent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            if (loadedNote!=null){
 
-                InputMethodManager methodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                assert methodManager != null;
-                methodManager.hideSoftInputFromWindow(Edbody.getWindowToken(), 0);
-                methodManager.hideSoftInputFromWindow(Edtitle.getWindowToken(), 1);
+                Edtitle.setText(loadedNote.getTitle());
+                Edbody.setText(loadedNote.getContent());
+                mydate.setText(String.valueOf(loadedNote.getDateTimeFormatted(this)));
 
             }
-        });
 
+        }
 
+        hideKeyboard();
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,25 +81,92 @@ public class AddActivity extends AppCompatActivity {
             NavUtils.navigateUpFromSameTask(this);
             break;
             case R.id.add_note:
-                Toast.makeText(getBaseContext(), "Note added", Toast.LENGTH_SHORT).show();
-                nextActivity();
+                saveNote();
                 break;
             case R.id.lock_note:
                 Toast.makeText(getBaseContext(), "Note locked", Toast.LENGTH_SHORT).show();
-                nextActivity();
                 break;
             case R.id.delete_note:
-                Toast.makeText(getBaseContext(), "Note deleted", Toast.LENGTH_SHORT).show();
-                nextActivity();
+               deleteNote();
                 break;
                 default:return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void nextActivity(){
-        Intent intent = new Intent(AddActivity.this, MainActivity.class);
-        startActivity(intent);
 
+    private void saveNote() {
+
+        if(Edtitle.getText().toString().trim().isEmpty() || Edbody.getText().toString().trim().isEmpty()){
+
+            Toast.makeText(getBaseContext(), "Empty note can't be saved", Toast.LENGTH_SHORT).show();
+        }else{
+
+        Note note;
+        if (loadedNote==null){
+
+            note = new Note(Edtitle.getText().toString(), Edbody.getText().toString(), System.currentTimeMillis());
+
+        }
+        else {
+
+            note = new Note(Edtitle.getText().toString(), Edbody.getText().toString(), loadedNote.getDate_time());
+
+        }
+
+        if(Utilitties.saveNote(this, note)){
+
+            Toast.makeText(getBaseContext(), "Note saved", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getBaseContext(), "Note not saved," +
+                    " please ensure you have enough space on your device", Toast.LENGTH_SHORT).show();
+        }
+        finish();
+        }
     }
+
+    private void deleteNote() {
+
+        if (loadedNote==null){
+
+            finish();
+        }else {
+
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                    .setTitle("Delete").setMessage("Do you really want to delete "+Edtitle.getText()+" ?")
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.dismiss();
+                        }
+                    }).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Utilitties.deleteNote(getApplicationContext(), loadedNote.getDate_time()+Utilitties.file_ext);
+                            Toast.makeText(getBaseContext(), Edtitle.getText().toString()+" deleted", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }).setIcon(R.drawable.ic_warning_black_24dp);
+            dialog.create().show();
+        }
+    }
+
+    private void hideKeyboard() {
+
+        parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                InputMethodManager methodManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                assert methodManager != null;
+                methodManager.hideSoftInputFromWindow(Edtitle.getWindowToken(), 0);
+                methodManager.hideSoftInputFromWindow(Edbody.getWindowToken(), 1);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/user/noteapp/MainActivity.java
+++ app/src/main/java/com/example/user/noteapp/MainActivity.java
@@ -1,30 +1,71 @@
 package com.example.user.noteapp;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private FloatingActionButton fab_add;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         fab_add = findViewById(R.id.fab_add);
+        listView = findViewById(R.id.list_view);
 
         fab_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this, AddActivity.class);
-                startActivity(intent);
+                Intent intent_fab = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent_fab);
             }
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listView.setAdapter(null);
 
+        ArrayList<Note> notes = Utilitties.getAllSavedNote(this);
+
+        if(notes==null || notes.size()==0){
+
+            Toast.makeText(getBaseContext(), "No saved note", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            final NoteAdapter adapter = new NoteAdapter(this, R.layout.note_item, notes);
+            listView.setAdapter(adapter);
+
+           listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+               @Override
+               public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                   String file_name = ((Note)listView.getItemAtPosition(position)).getDate_time()+Utilitties.file_ext;
+
+
+                           Intent intent = new Intent(getBaseContext(), ViewActivity.class);
+                           intent.putExtra("mFile", file_name);
+                           startActivity(intent);
+
+               }
+           });
+        }
+    }
 }
